e1c06a8aacd2abab75dc4ee8a7bbe014931c9b7f
Conan Shin <conan.cheolmin.shin@gmail.com>
2018-04-11 11:22:53 +0900

    basic vue component test using wrapper and instance

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
+import { shallow } from 'vue-test-utils'
 import HelloWorld from '@/components/HelloWorld'
 
 describe('HelloWorld.vue', () => {
+  let subjectWrapper, subjectInstance
+  beforeEach(() => {
+    subjectWrapper = shallow(HelloWorld)
+    subjectInstance = subjectWrapper.vm
+  })
+
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+    expect(subjectWrapper.contains('.hello h1.test-class')).toBeTruthy()
+  })
+
+  it('should call emit function with parameter', () => {
+    subjectInstance.emitTest()
+
+    expect(subjectWrapper.emitted().emitTest).toBeTruthy()
+    expect(subjectWrapper.emitted().emitTest.length).toEqual(1)
+    expect(subjectWrapper.emitted().emitTest[0]).toEqual(['emitTestParameter'])
   })
 })
